ee25dea11220c7f92f38216504082778bd3d913b
lsj <witmanjin@gmail.com>
2026-06-14 23:46:15 +0900

    Add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/samgukji3/',
   server: {
     open: true,
   },
