f36ce973d82b95993f4bc6f48905715812555d76
lsj <witmanjin@gmail.com>
2026-06-23 23:29:50 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,9 @@
+// 모든 캐시 삭제 후 자기 자신 해제
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
